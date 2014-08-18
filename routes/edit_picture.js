1bd143edb739e24700e605ffdfa4040dc971c002
Sufian Hassan <sufian.hassan.786@gmail.com>
2014-08-18 12:48:47 +0100

    cropping

--- routes/edit_picture.js
+++ routes/edit_picture.js
@@ -1,12 +1,12 @@
 var express 			= require('express'),
 		router 				= express.Router(),
-		cryptoToken 	= require('crypto').randomBytes,
+    cryptoToken   = require('crypto').randomBytes,
 		formidable    = require('formidable'),
     util          = require('util'),
     utils       	= require('../inc/utils'),
     gm 						= require('gm').subClass({ imageMagick: true }),
     fs            = require('fs-extra'),
-    model         = require('../model');
+    model         = require('../model'),
     getSlug       = require('speakingurl');
 
 
@@ -38,40 +38,62 @@ router.get('/:slug/edit', function(req, res, next) {
 
 router.post('/:slug/edit', function (req, res) {
   if (!req.isAuthenticated() )
-  	return res.redirect('/login');
+  	return res.send({redirect:'login'});
   
-  console.log(req.params.slug);
 	if (!req.params.slug)
-    return res.redirect('/');
+    return res.send({redirect:'home'});
   
-  model.getMyPicturesBySlugToken(req.user.id, req.params.slug, function(responce) {
+  model.getMyPicturesBySlugToken(req.user.id, req.params.slug, function(picture) {
 
-    if (responce[0]) {
+    if (picture[0]) {
 
-      var title   = req.body.title;
+      var title   = req.body.title,
+          cropX   = req.body.cropX,
+          cropY   = req.body.cropY,
+          cropW   = req.body.cropW,
+          cropH   = req.body.cropH;
 
-      if ( (!title.length > 2 || title.length > 80) ) {
-        return res.send({ status: 'invalid field(s)' });
+      if ( (!title.length > 2 || title.length > 81) ) {
+        return res.send({ status: 'Title Size must be between 3-80 Characters', field:'title' });
       }
+
+      // If all cropping fields are not provided then it will just reload the page.
+      if (!cropX || !cropY || !cropW || !cropH) return res.send({redirect:'/picture/'+req.params.slug+'/edit'});
+
+      var new_location  = 'public/uploads/cropped/',
+          file_name     = picture[0].picture;
       
-      var slug = getSlug(title)+'-'+cryptoToken(2).toString('hex');
+      // Gets original image, crops and resizes it.
+      gm('public/uploads/'+picture[0].picture).options({imageMagick: true})
+      .crop(cropW, cropH, cropX, cropY)
+      .resize(600)
+      .write(new_location+file_name, function (err) {
+        if (err) {
+          console.log(err);
+          return false
+        }
 
-      model.updateImageTitle(title, slug, responce[0].id, function(responce) {
-        console.log(responce)
-        if (!responce)
-          return res.send({ status: 'failed'});
-        
-        return res.redirect('/picture/'+slug);
+        var slug = getSlug(title)+'-'+cryptoToken(2).toString('hex'),
+            crop = JSON.stringify({'cropX':cropX, 'cropY':cropY, 'cropW':cropW, 'cropH':cropH});
 
+        model.updateImageTitle(title, slug, crop, picture[0].id, function(responce) {
+          console.log(responce)
+          if (!responce)
+            return res.send({ status: 'Something went wrong please try again.'});
+          
+          return res.send({redirect:'/picture/'+slug});
+
+        });
+
+
+        return res.send({redirect:'/picture/'+slug});
       });
 
-
+  
+      
+    
     }
   });
-  
-
-
-  // return res.redirect('/');
 
 });
 
